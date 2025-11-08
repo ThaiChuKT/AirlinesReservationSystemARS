76aca44b3791542da87e59b5dfae400fb284ddc1
Tsukine050824 <nhatnamx698@gmail.com>
2025-11-08 23:41:12 +0700

    database updated

--- arsdatabase (1).sql
+++ arsdatabase (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th10 08, 2025 lúc 09:56 AM
+-- Thời gian đã tạo: Th10 08, 2025 lúc 05:40 PM
 -- Phiên bản máy phục vụ: 10.4.32-MariaDB
 -- Phiên bản PHP: 8.2.12
 
@@ -101,7 +101,10 @@ CREATE TABLE `payments` (
 --
 
 INSERT INTO `payments` (`PaymentID`, `ReservationID`, `Amount`, `PaymentDate`, `PaymentMethod`, `TransactionStatus`, `TransactionRefNo`) VALUES
-(1, 3, 2500.00, '2025-11-08 08:53:39.413466', 'CreditCard', 'Completed', 'PG202511080853396544');
+(1, 3, 2500.00, '2025-11-08 08:53:39.413466', 'CreditCard', 'Completed', 'PAYTEST123'),
+(2, 3, 2500.00, '2025-11-08 16:38:27.313773', 'CreditCard', 'Completed', 'PG202511081638277066'),
+(3, 2, 2500.00, '2025-11-08 16:17:52.205296', 'CreditCard', 'Failed', 'PG202511081638486845'),
+(4, 5, 2500.00, '2025-11-08 16:37:33.974198', 'CreditCard', 'Pending', NULL);
 
 -- --------------------------------------------------------
 
@@ -168,7 +171,9 @@ CREATE TABLE `reservations` (
 
 INSERT INTO `reservations` (`ReservationID`, `UserID`, `FlightID`, `ScheduleID`, `BookingDate`, `TravelDate`, `Status`, `NumAdults`, `NumChildren`, `NumSeniors`, `Class`, `ConfirmationNumber`, `BlockingNumber`) VALUES
 (2, 3, 3, 1, '2025-11-08', '2025-10-30', 'Confirmed', 1, 0, 0, 'Economy', 'ARS202511080744046686', NULL),
-(3, 3, 3, 1, '2025-11-08', '2025-10-30', 'Confirmed', 1, 0, 0, 'Economy', 'ARS202511080833469467', NULL);
+(3, 3, 3, 1, '2025-11-08', '2025-10-30', 'Confirmed', 1, 0, 0, 'Economy', 'ARS202511080833469467', NULL),
+(4, 3, 3, 1, '2025-11-08', '2025-10-30', 'Cancelled', 1, 0, 0, 'Economy', 'ARS202511081604368390', NULL),
+(5, 3, 3, 1, '2025-11-08', '2025-10-30', 'Blocked', 1, 0, 0, 'Economy', 'ARS202511081624294635', 'BLK20251108162429206');
 
 -- --------------------------------------------------------
 
@@ -330,7 +335,7 @@ ALTER TABLE `flights`
 -- AUTO_INCREMENT cho bảng `payments`
 --
 ALTER TABLE `payments`
-  MODIFY `PaymentID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `PaymentID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT cho bảng `pricingpolicies`
@@ -348,7 +353,7 @@ ALTER TABLE `refunds`
 -- AUTO_INCREMENT cho bảng `reservations`
 --
 ALTER TABLE `reservations`
-  MODIFY `ReservationID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `ReservationID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT cho bảng `schedules`
