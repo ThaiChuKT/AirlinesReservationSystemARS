a04a41934854d37a2b3e8e1148543e4e76b9f009
Lê Nhật Nam <166350221+Tsukine050824@users.noreply.github.com>
2025-11-26 01:18:40 +0700

    images

--- ARS/obj/Debug/net9.0/ARS.AssemblyInfo.cs
+++ ARS/obj/Debug/net9.0/ARS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ARS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f84259957ecfafc403aa7aef15bdbc16117d169f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bed997887d920a46e26bfbccd0ec84fb9af170b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ARS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ARS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
