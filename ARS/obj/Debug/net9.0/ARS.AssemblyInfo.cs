bed997887d920a46e26bfbccd0ec84fb9af170b5
Lê Nhật Nam <166350221+Tsukine050824@users.noreply.github.com>
2025-11-25 23:05:02 +0700

    header footer fixed

--- ARS/obj/Debug/net9.0/ARS.AssemblyInfo.cs
+++ ARS/obj/Debug/net9.0/ARS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ARS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbdb6d366ee9f2c47053377b70c06897ccbabb41")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f84259957ecfafc403aa7aef15bdbc16117d169f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ARS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ARS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
